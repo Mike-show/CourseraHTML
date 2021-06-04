d8f0937fe6ddd755b64207dcf88380f7c97fde7e
Mike-show <76592022+Mike-show@users.noreply.github.com>
2021-06-04 15:20:10 +0300

    module3 modified

--- module4/script.js
+++ module4/script.js
@@ -57,7 +57,7 @@ WARNING!!! WARNING!!!
      // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
      // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
      // name in the loop.
-     if (firstLetter.toLowerCase() != 'j') {
+     if (firstLetter.toLowerCase() == 'j') {
        byeSpeaker.sayGoodbye(names[prop]);
      } else {
        helloSpeaker.sayHello(names[prop]);
